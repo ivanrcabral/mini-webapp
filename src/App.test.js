64bfae797f4c860cf3d11da19bb251199ab97157
Ivan Cabral <cabral.developer@gmail.com>
2020-05-13 20:35:22 +0200

    base project

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,59 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import Header from "./App/Components/Header/header";
+import Home from "./App/Components/Home/home";
+import Movies from "./App/Components/Home/movies";
+import Movie from "./App/Components/Movie/movie";
+import MovieDetail from "./App/Components/Movie/movieDetail";
+
+
+describe("<Header />", () => {
+
+  const component = shallow(<Header />);
+
+  it("should render a <Header /> component", () => {
+      expect(component).toBeDefined();
+  }); 
+
+});
+
+describe("<Home />", () => {
+
+    const component = shallow(<Home />);
+
+    it("should render a <Home /> component", () => {
+        expect(component).toBeDefined();
+    }); 
+
+});
+
+describe("<Movies />", () => {
+
+  const component = shallow(<Movies />);
+
+  it("should render a <Movies /> component", () => {
+      expect(component).toBeDefined();
+  }); 
+
+});
+
+describe("<Movie />", () => {
+
+  const component = shallow(<Movie />);
+
+  it("should render a <Movie /> component", () => {
+      expect(component).toBeDefined();
+  }); 
+
+});
+
+describe("<MovieDetail />", () => {
+
+  const component = shallow(<MovieDetail />);
+
+  it("should render a <MovieDetail /> component", () => {
+      expect(component).toBeDefined();
+  }); 
+
 });
